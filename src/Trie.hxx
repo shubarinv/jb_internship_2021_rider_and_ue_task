f4cacb558a8b526c7ec2250482d5a8a83109113d
Vladimir Shubarin <vhundef@gmail.com>
2021-04-03 15:07:07 +0300

    Added findWordRecursive to Trie class

--- src/Trie.hxx
+++ src/Trie.hxx
@@ -24,10 +24,10 @@ public:
         if (t_word.empty()) {
             std::cerr << "Trie(" << this << ")::addWord(): Attempt to add empty word! (word will be ignored)" << std::endl;
         }
-        if (!root->hasChild(t_word[0])) {                    ///< checking if root node has a child node with a value that is equal to first char in given word
-            root->children().push_front(new Node(t_word[0]));///< node doesn't have child with required value, so we add it here
+        if (!root->hasChild(t_word[0])) {                    // checking if root node has a child node with a value that is equal to first char in given word
+            root->children().push_front(new Node(t_word[0]));// node doesn't have child with required value, so we add it here
         }
-        addWord(t_word.substr(1, t_word.size()), root->getChild(t_word[0]));///< by this time we can be sure that there is a child whose val. is equal to word's first character. We will now switch to this child
+        addWord(t_word.substr(1, t_word.size()), root->getChild(t_word[0]));// by this time we can be sure that there is a child whose val. is equal to word's first character. We will now switch to this child
     }
 
     void findWord(std::string word) {
@@ -41,15 +41,34 @@ private:
     Node *root{nullptr};
 
     static void addWord(std::string word, Node *node) {
-        if (word[0] == '\0') return;                           ///< Prevents \0 from being added from the end of the word
-        if (!word.empty()) {                                   ///< checking if there is anything to add
-            if (!node->hasChild(word[0])) {                    ///< checking if current node doesn't have child with value that is same as first char of given word
+        if (word[0] == '\0') return;                           // Prevents \0 from being added from the end of the word
+        if (!word.empty()) {                                   // checking if there is anything to add
+            if (!node->hasChild(word[0])) {                    // checking if current node doesn't have child with value that is same as first char of given word
                 node->children().push_front(new Node(word[0]));///< adding missing child
             }
-            addWord(word.substr(1, word.size()), node->getChild(word[0]));///< switching to node that has next char from word
+            addWord(word.substr(1, word.size()), node->getChild(word[0]));// switching to node that has next char from word
         }
     }
 
+    static void findWordRecursive(Node *node, std::string word, std::string result) {
+        if (node == nullptr) {
+            std::cerr << "Error: Trie::findWordRecursive: node somewhy empty";
+            return;
+        }
+
+        if (node->value() != '\0')// prevents root node from being add to resulting string(there will be problems outputting it otherwise)
+            result += node->value();
+
+        if (node->value() == word[0])// if value of current node equal word first char, we remove this char from word
+            word = word.substr(1, word.size());
+
+        if (node->children().empty() && word.empty()) {// if word was found, and we reached the end of branch
+            std::cout << result;
+        }
+        for (auto &childNode : node->children()) {// recursively going through the trie
+            findWordRecursive(childNode, word, result);
+        }
+    }
 };
 
 
